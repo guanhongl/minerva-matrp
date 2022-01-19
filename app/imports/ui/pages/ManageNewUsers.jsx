34a060ca7eccc91729c63d5d12bc7776e4283003
guanhongl <guanh.sl23@gmail.com>
2022-01-19 01:08:34 -1000

    finish reject user

--- app/imports/ui/pages/ManageNewUsers.jsx
+++ app/imports/ui/pages/ManageNewUsers.jsx
@@ -2,8 +2,39 @@ import React from 'react';
 import { Container, Table, Header, Loader, Button } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
+// import { Accounts } from 'meteor/accounts-base';
 import { PendingUsers } from '../../api/PendingUserCollection';
+import { removeItMethod } from '../../api/base/BaseCollection.methods';
 // import { PAGE_IDS } from '../utilities/PageIDs';
+
+const acceptUser = ({ firstName, lastName, email }) => {
+
+};
+
+const rejectUser = (email, id) => {
+  swal({
+    title: 'Are you sure?',
+    text: `Do you really want to reject ${email}?`,
+    icon: 'warning',
+    buttons: [
+      'No, cancel it!',
+      'Yes, I am sure!',
+    ],
+    dangerMode: true,
+  })
+    .then((isConfirm) => {
+      // if 'yes'
+      if (isConfirm) {
+        const collectionName = PendingUsers.getCollectionName();
+
+        removeItMethod.callPromise({ collectionName, instance: id })
+          .catch(error => swal('Error', error.message, 'error'))
+          .then(() => {
+            swal('Success', `${email} rejected successfully`, 'success', { buttons: false, timer: 3000 });
+          });
+      }
+    });
+};
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 const ManageNewUsers = ({ ready, users }) => ((ready) ? (
@@ -27,8 +58,8 @@ const ManageNewUsers = ({ ready, users }) => ((ready) ? (
               <Table.Cell>{user.lastName}</Table.Cell>
               <Table.Cell>{user.email}</Table.Cell>
               <Table.Cell>
-                <Button positive>Accept</Button>
-                <Button negative>Reject</Button>
+                <Button positive onClick={() => acceptUser(user)}>Accept</Button>
+                <Button negative onClick={() => rejectUser(user.email, user._id)}>Reject</Button>
               </Table.Cell>
             </Table.Row>
           )
