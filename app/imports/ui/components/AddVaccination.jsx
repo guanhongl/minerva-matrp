46893db6a41f98e6d02c12a6fe5108dfe6b2b3b5
guanhongl <guanh.sl23@gmail.com>
2022-02-10 00:47:02 -1000

    implement add vaccine

--- app/imports/ui/components/AddVaccination.jsx
+++ app/imports/ui/components/AddVaccination.jsx
@@ -1,53 +1,49 @@
 import React, { useState, useEffect } from 'react';
 import { Grid, Header, Form, Button, Tab, Loader } from 'semantic-ui-react';
-import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { _ } from 'meteor/underscore';
 import swal from 'sweetalert';
-import { Sites } from '../../api/site/SiteCollection';
 import { Locations } from '../../api/location/LocationCollection';
 import { COMPONENT_IDS } from '../utilities/ComponentIDs';
 import { Vaccinations } from '../../api/vaccination/VaccinationCollection';
-import { DrugTypes } from '../../api/drugType/DrugTypeCollection';
 import { distinct, getOptions, nestedDistinct } from '../utilities/Functions';
 import { defineMethod, updateMethod } from '../../api/base/BaseCollection.methods';
 
 /** On submit, insert the data. */
 const submit = (data, callback) => {
-  const { vaccine, minQuantity, quantity, visDate, brand, lotId, expire, location, donated, donatedBy, note } = data;
+  const { vaccine, minQuantity, quantity, visDate, brand, lotId, expire, location, note } = data;
   const collectionName = Vaccinations.getCollectionName();
-  const exists = Vaccinations.findOne({ vaccine }); // returns the existing medication or undefined
+  const exists = Vaccinations.findOne({ vaccine }); // returns the existing vaccine or undefined
 
-  // if the medication does not exist:
+  // if the vaccine does not exist:
   if (!exists) {
-    // insert the new medication and lotId
-    const newLot = { lotId, expire, location, quantity, donated, donatedBy, note };
+    // insert the new vaccine and lotId
+    const newLot = { lotId, expire, location, quantity, note };
     const definitionData = { vaccine, brand, minQuantity, visDate, lotIds: [newLot] };
     defineMethod.callPromise({ collectionName, definitionData })
-      .catch(error => swal('Error', error.message, 'error'))
       .then(() => {
         swal('Success', `${vaccine}, ${lotId} added successfully`, 'success', { buttons: false, timer: 3000 });
         callback(); // resets the form
-      });
+      })
+      .catch(error => swal('Error', error.message, 'error'));
   } else {
     const { lotIds } = exists;
-    // const targetIndex = lotIds.findIndex((obj => obj.lotId === lotId));
     const target = lotIds.find(obj => obj.lotId === lotId);
     // if lotId exists, increment the quantity:
     if (target) {
       target.quantity += quantity;
     } else {
       // else append the new lotId
-      lotIds.push({ lotId, brand, expire, visDate, location, quantity, donated, donatedBy, note });
+      lotIds.push({ lotId, expire, location, quantity, note });
     }
     const updateData = { id: exists._id, lotIds };
     updateMethod.callPromise({ collectionName, updateData })
-      .catch(error => swal('Error', error.message, 'error'))
       .then(() => {
         swal('Success', `${vaccine} updated successfully`, 'success', { buttons: false, timer: 3000 });
         callback(); // resets the form
-      });
+      })
+      .catch(error => swal('Error', error.message, 'error'));
   }
 };
 
@@ -56,11 +52,11 @@ const validateForm = (data, callback) => {
   const submitData = { ...data };
   let errorMsg = '';
   // the required String fields
-  const requiredFields = ['vaccine', 'brand', 'minQuantity', 'visDate', 'lotId', 'brand', 'location', 'quantity'];
+  const requiredFields = ['vaccine', 'brand', 'minQuantity', 'visDate', 'lotId', 'location', 'quantity'];
 
   // if the field is empty, append error message
   requiredFields.forEach(field => {
-    if (!submitData[field] || (field === 'vaccine' && !submitData.vaccine.length)) {
+    if (!submitData[field]) {
       errorMsg += `${field} cannot be empty.\n`;
     }
   });
@@ -74,7 +70,7 @@ const validateForm = (data, callback) => {
   }
 };
 
-/** Renders the Page for Dispensing Inventory. */
+/** Renders the Page for Add Vaccine */
 const AddVaccination = ({ ready, vaccines, locations, lotIds, brands }) => {
   const [fields, setFields] = useState({
     vaccine: '',
@@ -85,8 +81,6 @@ const AddVaccination = ({ ready, vaccines, locations, lotIds, brands }) => {
     expire: '',
     location: '',
     quantity: '',
-    donated: false,
-    donatedBy: '',
     note: '',
   });
   const isDisabled = vaccines.includes(fields.vaccine);
@@ -98,16 +92,16 @@ const AddVaccination = ({ ready, vaccines, locations, lotIds, brands }) => {
   }, [vaccines]);
   const [filteredVaccines, setFilteredVaccines] = useState([]);
   useEffect(() => {
-    setFilteredVaccines(vaccines);
-  }, [vaccines]);
+    setFilteredVaccines(newVaccines);
+  }, [newVaccines]);
   const [newBrands, setNewBrands] = useState([]);
   useEffect(() => {
     setNewBrands(brands);
   }, [brands]);
   const [filteredBrands, setFilteredBrands] = useState([]);
   useEffect(() => {
-    setFilteredBrands(brands);
-  }, [brands]);
+    setFilteredBrands(newBrands);
+  }, [newBrands]);
   const [newLotIds, setNewLotIds] = useState([]);
   useEffect(() => {
     setNewLotIds(lotIds);
@@ -129,7 +123,6 @@ const AddVaccination = ({ ready, vaccines, locations, lotIds, brands }) => {
       setNewLotIds([...newLotIds, value]);
     }
   };
-
   // handles adding a new brand; IS case sensitive
   const onAddBrand = (event, { value }) => {
     if (!newBrands.map(brand => brand.toLowerCase()).includes(value.toLowerCase())) {
@@ -141,32 +134,22 @@ const AddVaccination = ({ ready, vaccines, locations, lotIds, brands }) => {
     setFields({ ...fields, [name]: value });
   };
 
-  const handleCheck = (event, { name, checked }) => {
-    if (!checked) {
-      setFields({ ...fields, [name]: checked, donatedBy: '' });
-    } else {
-      setFields({ ...fields, [name]: checked });
-    }
-  };
-
   // handles vaccine select
   const onVaccineSelect = (event, { value: vaccine }) => {
     const target = Vaccinations.findOne({ vaccine });
-    // if the drug exists:
+    // if the vaccine exists:
     if (target) {
       // autofill the form with specific vaccine info
-      const { minQuantity, lotIds: lotIdObjs } = target;
-      setFields({ ...fields, vaccine, minQuantity });
+      const { brand, minQuantity, visDate, lotIds: lotIdObjs } = target;
+      setFields({ ...fields, vaccine, brand, minQuantity, visDate });
       // filter lotIds and brands
       // TODO: sort?
       setFilteredLotIds(_.pluck(lotIdObjs, 'lotId'));
     } else {
-      // else reset specific drug info
-      setFields({ ...fields, vaccine, minQuantity: '', expire: '', visDate: '',
-        brand: '', location: '', donated: false, donatedBy: '', note: '' });
+      // else reset specific vaccine info
+      setFields({ ...fields, vaccine, minQuantity: '', brand: '', visDate: '' });
       // reset the filters
       setFilteredLotIds(newLotIds);
-      setFilteredBrands(newBrands);
     }
   };
 
@@ -178,32 +161,18 @@ const AddVaccination = ({ ready, vaccines, locations, lotIds, brands }) => {
       // autofill the form with specific lotId info
       const targetLotIds = target.lotIds.find(obj => obj.lotId === lotId);
       const { vaccine, minQuantity, brand, visDate } = target;
-      const { expire, location, donated, donatedBy, note } = targetLotIds;
-      const autoFields = { ...fields, lotId, vaccine, expire, brand, visDate, minQuantity, location,
-        donated, donatedBy, note };
+      const { expire, location, note } = targetLotIds;
+      const autoFields = { ...fields, lotId, vaccine, expire, brand, visDate, minQuantity, location, note };
       setFields(autoFields);
     } else {
       // else reset specific lotId info
-      setFields({ ...fields, lotId, expire: '', visDate: '', location: '', donated: false, donatedBy: '', note: '' });
-      setFilteredBrands(newBrands);
-    }
-  };
-
-  // handles brand select
-  const onBrandSelect = (event, { value: brand }) => {
-    setFields({ ...fields, brand });
-    // filter vaccines
-    const filter = distinct('vaccine', Vaccinations, { brand });
-    if (filter.length && !fields.vaccine) {
-      setFilteredVaccines(filter);
-    } else {
-      setFilteredVaccines(newVaccines);
+      setFields({ ...fields, lotId, expire: '', location: '', note: '' });
     }
   };
 
   const clearForm = () => {
     setFields({ vaccine: '', brand: '', minQuantity: '', visDate: '', lotId: '', expire: '',
-      location: '', quantity: '', donated: false, donatedBy: '', note: '' });
+      location: '', quantity: '', note: '' });
     setFilteredVaccines(newVaccines);
     setFilteredLotIds(newLotIds);
     setFilteredBrands(newBrands);
@@ -216,7 +185,7 @@ const AddVaccination = ({ ready, vaccines, locations, lotIds, brands }) => {
           <Header.Content>
               Add Vaccine to Inventory Form
             <Header.Subheader>
-              <i>Please input all relative fields to add a vaccine to the inventory</i>
+              <i>Please input the following information to add to the inventory, to the best of your abilities.</i>
             </Header.Subheader>
           </Header.Content>
         </Header>
@@ -225,7 +194,7 @@ const AddVaccination = ({ ready, vaccines, locations, lotIds, brands }) => {
             <Grid.Row>
               <Grid.Column>
                 <Form.Select clearable search label='Vaccine Name'
-                  placeholder=" J&J COVID" name='vaccine' options={getOptions(filteredVaccines)}
+                  placeholder="J&J COVID" name='vaccine' options={getOptions(filteredVaccines)}
                   onChange={onVaccineSelect} value={fields.vaccine}
                   allowAdditions onAddItem={onAddVaccine} id={COMPONENT_IDS.ADD_VACCINATION_VACCINE}/>
               </Grid.Column>
@@ -233,19 +202,21 @@ const AddVaccination = ({ ready, vaccines, locations, lotIds, brands }) => {
               <Grid.Column className='filler-column' />
             </Grid.Row>
             <Grid.Row>
-              {/* TODO: expand drug type column */}
               <Grid.Column>
                 <Form.Select clearable search label='Manufacturer Brand'
                   placeholder="ACAM2000 Sanofi Pasteur" options={getOptions(filteredBrands)}
-                  name='brand' onChange={onBrandSelect} value={fields.brand}
-                  allowAdditions onAddItem={onAddBrand} id={COMPONENT_IDS.ADD_VACCINATION_BRAND}/>
+                  name='brand' onChange={handleChange} value={fields.brand}
+                  allowAdditions onAddItem={onAddBrand} id={COMPONENT_IDS.ADD_VACCINATION_BRAND} disabled={isDisabled}/>
               </Grid.Column>
               <Grid.Column>
                 <Form.Input label='Minimum Quantity' type='number' min={1} name='minQuantity' className='quantity'
                   onChange={handleChange} value={fields.minQuantity} disabled={isDisabled}
                   placeholder="100" id={COMPONENT_IDS.ADD_VACCINATION_MIN_QUANTITY}/>
               </Grid.Column>
-              <Grid.Column className='filler-column'/>
+              <Grid.Column>
+                <Form.Input type='date' label='VIS Date' name='visDate'
+                  onChange={handleChange} value={fields.visDate} disabled={isDisabled}/>
+              </Grid.Column>
             </Grid.Row>
             <Grid.Row>
               <Grid.Column>
@@ -258,11 +229,7 @@ const AddVaccination = ({ ready, vaccines, locations, lotIds, brands }) => {
                 <Form.Input type='date' label='Expiration Date' name='expire'
                   onChange={handleChange} value={fields.expire} id={COMPONENT_IDS.ADD_VACCINATION_EXPIRATION}/>
               </Grid.Column>
-              <Grid.Column>
-                {/* expiration date may be null */}
-                <Form.Input type='date' label='VIS Date' name='visDate'
-                  onChange={handleChange} value={fields.visDate}/>
-              </Grid.Column>
+              <Grid.Column className='filler-column'/>
             </Grid.Row>
             <Grid.Row>
               <Grid.Column>
@@ -275,17 +242,7 @@ const AddVaccination = ({ ready, vaccines, locations, lotIds, brands }) => {
                   onChange={handleChange} value={fields.quantity} placeholder="200"
                   id={COMPONENT_IDS.ADD_VACCINATION_QUANTITY}/>
               </Grid.Column>
-              <Grid.Column>
-                <Form.Field>
-                  <label>Donated</label>
-                  <Form.Group>
-                    <Form.Checkbox name='donated' className='donated-field'
-                      onChange={handleCheck} checked={fields.donated} id={COMPONENT_IDS.ADD_VACCINATION_DONATED}/>
-                    <Form.Input name='donatedBy' className='donated-by-field' placeholder='Donated By'
-                      onChange={handleChange} value={fields.donatedBy} disabled={!fields.donated} id={COMPONENT_IDS.ADD_VACCINATION_DONATED_INPUT}/>
-                  </Form.Group>
-                </Form.Field>
-              </Grid.Column>
+              <Grid.Column className='filler-column'/>
             </Grid.Row>
             <Grid.Row>
               <Grid.Column>
@@ -309,8 +266,6 @@ const AddVaccination = ({ ready, vaccines, locations, lotIds, brands }) => {
 
 /** Require an array of Stuff documents in the props. */
 AddVaccination.propTypes = {
-  currentUser: PropTypes.object,
-  sites: PropTypes.array.isRequired,
   vaccines: PropTypes.array.isRequired,
   lotIds: PropTypes.array.isRequired,
   locations: PropTypes.array.isRequired,
@@ -321,17 +276,14 @@ AddVaccination.propTypes = {
 // Currently vaccination subscribes to same drugType collection as medication collection.
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(() => {
-  const typeSub = DrugTypes.subscribeDrugType();
   const locationSub = Locations.subscribeLocation();
   const vacSub = Vaccinations.subscribeVaccination();
   return {
-    currentUser: Meteor.user(),
-    sites: Sites.find({}).fetch(),
     vaccines: distinct('vaccine', Vaccinations),
     lotIds: nestedDistinct('lotId', Vaccinations),
     locations: distinct('location', Locations),
     brands: distinct('brand', Vaccinations),
-    ready: typeSub.ready() && locationSub.ready() && vacSub.ready(),
+    ready: locationSub.ready() && vacSub.ready(),
 
   };
 })(AddVaccination);
