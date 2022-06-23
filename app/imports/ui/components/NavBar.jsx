4261e886a374b2f2c372275793cff8621b270e83
guanhongl <guanh.sl23@gmail.com>
2022-06-22 17:27:13 -1000

    update navbar

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -11,197 +11,125 @@ import StatusNotification from '../pages/StatusNotification';
 
 /** The NavBar appears at the top of every page. Rendered by the App Layout component. */
 const NavBar = ({ currentUser }) => {
-  const menuStyle = { marginBottom: '30px', backgroundColor: '#1D3E66', backgroundSize: 'content' };
   const [mobile, setMobile] = useState(false);
 
-  // TODO: fix event listener
-  // const handleMobile = () => {
-  //   if (window.innerWidth < 720) {
-  //     setMobile(true);
-  //   } else {
-  //     setMobile(true);
-  //   }
-  // };
+  const handleMobile = e => {
+    // console.log(window.innerWidth)
+    if (window.innerWidth < 720) {
+      setMobile(true);
+    } else {
+      setMobile(false);
+    }
+  };
 
-  // useEffect(() => {
-  //   window.addEventListener('resize', handleMobile);
-  // });
+  useEffect(() => {
+    window.addEventListener("resize", handleMobile);
+    return () => {
+      window.removeEventListener("resize", handleMobile);
+    };
+  }, [handleMobile]);
 
   if (mobile) {
     return (
-      <Menu style={menuStyle} attached="top" borderless inverted compact>
+      <Menu id="nav" attached="top" borderless inverted compact>
         <Menu.Item id={COMPONENT_IDS.NAVBAR_LANDING_PAGE} as={NavLink} activeClassName="" exact to="/about">
           <Header inverted as='h1'>Minerva</Header>
         </Menu.Item>
         {
-          currentUser ? (
-            [
-              <Menu.Item key='dropdown-navbar' fitted>
-                <Dropdown icon='bars'>
-                  <Dropdown.Menu>
-                    <Dropdown.Item id={COMPONENT_IDS.NAVBAR_ABOUT_US} as={NavLink} activeClassName="active" exact
-                      to="/about"
-                      key='list' content="About us"/>
-                    <Dropdown.Item id={COMPONENT_IDS.NAVBAR_ADD_INVENTORY} as={NavLink} activeClassName="active"
-                      exact
-                      to="/add"
-                      key='add' position="right" content="Add to Inventory"/>
-                    <Dropdown.Item id={COMPONENT_IDS.NAVBAR_DISPENSE} as={NavLink} activeClassName="active" exact
-                      to="/dispense"
-                      key='dispense' content="Dispense Inventory"/>
-                    <Dropdown.Item id={COMPONENT_IDS.NAVBAR_STATUS} as={NavLink} activeClassName="active" exact
-                      to="/status"
-                      key='status' content='Inventory Status'/>
-                    <Dropdown.Item id={COMPONENT_IDS.NAVBAR_DISPENSE_LOG} as={NavLink} activeClassName="active"
-                      exact
-                      to="/dispense-log" key='dispense-log' content='Dispense Log'/>
-                    <Dropdown.Item id={COMPONENT_IDS.NAVBAR_MANAGE_DROPDOWNS} as={NavLink} activeClassName="active"
-                      exact
-                      to="/manage-dropdowns" key='manage-dropdowns' content='Manage Dropdowns'/>
-                  </Dropdown.Menu>
-                </Dropdown>
-              </Menu.Item>,
-            ]
-          ) : ''
+          !!currentUser &&
+          <Dropdown item icon='bars' key='bars'>
+            <Dropdown.Menu>
+              <Dropdown.Item id={COMPONENT_IDS.NAVBAR_ADD_INVENTORY} as={NavLink} activeClassName="active"
+                exact to="/add" key='add' content="Add Inventory"/>
+              <Dropdown.Item id={COMPONENT_IDS.NAVBAR_DISPENSE} as={NavLink} activeClassName="active" 
+                exact to="/dispense" key='dispense' content="Dispense Inventory"/>
+              <Dropdown.Item id={COMPONENT_IDS.NAVBAR_STATUS} as={NavLink} activeClassName="active" 
+                exact to="/status" key='status' content='Inventory Status'/>
+              <Dropdown.Item id={COMPONENT_IDS.NAVBAR_DISPENSE_LOG} as={NavLink} activeClassName="active"
+                exact to="/dispense-log" key='dispense-log' content='Dispense Log'/>
+            </Dropdown.Menu>
+          </Dropdown>
         }
-        {Roles.userIsInRole(Meteor.userId(), [ROLE.ADMIN]) ? (
-          [<Menu.Item key='admin' fitted>
-            <Dropdown id={COMPONENT_IDS.NAVBAR_MANAGE_DROPDOWN} key="manage-dropdown" icon='setting'>
-              <Dropdown.Menu>
-                <Dropdown.Item id={COMPONENT_IDS.NAVBAR_LIST_STUFF_ADMIN} as={NavLink} activeClassName="active" exact
-                  to="/admin" key='admin' content='Admin'/>
-                {/* <Dropdown.Item id={COMPONENT_IDS.NAVBAR_MANAGE_DROPDOWN_DATABASE} key="manage-database"
-                  as={NavLink} exact to="/manage-database" content="Database"/> */}
-                <Dropdown.Item id={COMPONENT_IDS.NAVBAR_MANAGE_DROPDOWN_USERS} key="manage-users"
-                  as={NavLink} exact to="/manage-users" content="Users"/>
-                <Dropdown.Item key="manage-new-users" as={NavLink} exact to="/manage-new-users" content="New Users"/>
-              </Dropdown.Menu>
-            </Dropdown>
-          </Menu.Item>]
-        ) : ''}
-        <Menu.Item id={COMPONENT_IDS.NAVBAR_STATUS_NOTIFICATION} size='tiny' fitted>
+        {
+          Roles.userIsInRole(Meteor.userId(), [ROLE.ADMIN]) &&
+          <Dropdown id={COMPONENT_IDS.NAVBAR_MANAGE_DROPDOWN} item key="setting" icon='setting'>
+            <Dropdown.Menu>
+              <Dropdown.Item id={COMPONENT_IDS.NAVBAR_MANAGE_DROPDOWNS} as={NavLink} exact to="/manage-dropdowns" key='manage-dropdowns' content="Dropdowns" />
+              <Dropdown.Item id={COMPONENT_IDS.NAVBAR_MANAGE_DROPDOWN_DATABASE} key="manage-database" as={NavLink} exact to="/manage-database" content="Database" />
+              <Dropdown.Item id={COMPONENT_IDS.NAVBAR_MANAGE_DROPDOWN_USERS} key="manage-users" as={NavLink} exact to="/manage-users" content="Users"/>
+              <Dropdown.Item key="manage-new-users" as={NavLink} exact to="/manage-new-users" content="New Users"/>
+            </Dropdown.Menu>
+          </Dropdown>
+        }
+        {/* <Menu.Item id={COMPONENT_IDS.NAVBAR_STATUS_NOTIFICATION} position="right">
           <StatusNotification/>
-        </Menu.Item>
-        <Menu.Item position="right" fitted>
-          {
-            currentUser === '' ?
-              (
-                <Dropdown id={COMPONENT_IDS.NAVBAR_LOGIN_DROPDOWN} text="Login" pointing="top right"
-                  icon={'user md'}>
-                  <Dropdown.Menu>
-                    <Dropdown.Item id={COMPONENT_IDS.NAVBAR_LOGIN_DROPDOWN_SIGN_IN} icon="user" text="Sign In"
-                      as={NavLink} exact to="/signin"/>
-                    <Dropdown.Item id={COMPONENT_IDS.NAVBAR_LOGIN_DROPDOWN_SIGN_UP} icon="add user"
-                      text="Sign Up"
-                      as={NavLink} exact to="/signup"/>
-                  </Dropdown.Menu>
-                </Dropdown>
-              ) :
-              (
-                <Dropdown id={COMPONENT_IDS.NAVBAR_CURRENT_USER} text={currentUser} pointing="top right"
-                  icon={'user'}>
-                  <Dropdown.Menu>
-                    <Dropdown.Item id={COMPONENT_IDS.NAVBAR_SIGN_OUT} icon="sign out" text="Sign Out"
-                      as={NavLink} exact
-                      to="/signout"/>
-                  </Dropdown.Menu>
-                </Dropdown>
-              )
-          }
+        </Menu.Item> */}
+        <Menu.Item position='right'>
+          <Dropdown id={COMPONENT_IDS.NAVBAR_CURRENT_USER} text={currentUser} pointing="top right" icon={'user'}>
+            <Dropdown.Menu>
+              <Dropdown.Item id={COMPONENT_IDS.NAVBAR_SIGN_OUT} icon="sign out" text="Sign Out" as={NavLink} exact to="/signout"/>
+            </Dropdown.Menu>
+          </Dropdown>
         </Menu.Item>
       </Menu>
     );
   }
 
   return (
-    <div className='nav'>
-      <Menu style={menuStyle} attached="top" borderless stackable inverted compact>
-        <Menu.Item id={COMPONENT_IDS.NAVBAR_LANDING_PAGE} as={NavLink} activeClassName="" exact to="/about">
-          <Header inverted as='h1'>Minerva</Header>
-        </Menu.Item>
-        {
-          currentUser ? (
-            [
-              <Menu.Item id={COMPONENT_IDS.NAVBAR_ADD_INVENTORY} as={NavLink} activeClassName="active" exact
-                to="/add"
-                key='add' position="right">
-                    Add to Inventory
-                <Icon name='plus'/>
-              </Menu.Item>,
-              <Menu.Item id={COMPONENT_IDS.NAVBAR_DISPENSE} as={NavLink} activeClassName="active" exact
-                to="/dispense"
-                key='dispense'>
-                    Dispense Inventory
-                <Icon name='pills'/>
-              </Menu.Item>,
-              <Menu.Item id={COMPONENT_IDS.NAVBAR_STATUS} as={NavLink} activeClassName="active" exact to="/status"
-                key='status'>
-                    Inventory Status
-                <Icon name='archive'/>
-              </Menu.Item>,
-              <Menu.Item id={COMPONENT_IDS.NAVBAR_DISPENSE_LOG} as={NavLink} activeClassName="active" exact
-                to="/dispense-log" key='dispense-log'>
-                    Dispense Log
-                <Icon name='book'/>
-              </Menu.Item>,
-              <Menu.Item id={COMPONENT_IDS.NAVBAR_MANAGE_DROPDOWNS} as={NavLink} activeClassName="active" exact
-                to="/manage-dropdowns" key='manage-dropdowns'>
-                    Manage Dropdowns
-              </Menu.Item>,
-            ]
-          ) : ''
-        }
-        {Roles.userIsInRole(Meteor.userId(), [ROLE.ADMIN]) ? (
-          [
-            <Dropdown id={COMPONENT_IDS.NAVBAR_MANAGE_DROPDOWN} item text="Manage" key="manage-dropdown">
-              <Dropdown.Menu>
-                <Dropdown.Item id={COMPONENT_IDS.NAVBAR_MANAGE_DROPDOWN_DATABASE} key="manage-database" as={NavLink} exact to="/manage-database" content="Database" />
-                <Dropdown.Item id={COMPONENT_IDS.NAVBAR_MANAGE_DROPDOWN_USERS} key="manage-users"
-                  as={NavLink} exact to="/manage-users" content="Users"/>
-                <Dropdown.Item key="manage-new-users" as={NavLink} exact to="/manage-new-users" content="New Users"/>
-              </Dropdown.Menu>
-            </Dropdown>]
-        ) : ''}
-        <Menu.Item position="right">
-          <Menu.Item id={COMPONENT_IDS.NAVBAR_STATUS_NOTIFICATION}>
-            <StatusNotification/>
+    <Menu id="nav" attached="top" borderless inverted compact>
+      <Menu.Item id={COMPONENT_IDS.NAVBAR_LANDING_PAGE} as={NavLink} activeClassName="" exact to="/about">
+        <Header inverted as='h1'>Minerva</Header>
+      </Menu.Item>
+      {
+        !!currentUser &&
+        <>
+          <Menu.Item id={COMPONENT_IDS.NAVBAR_ADD_INVENTORY} as={NavLink} activeClassName="active" exact to="/add" key='add'>
+            Add Inventory
+            <Icon name='plus'/>
           </Menu.Item>
-          {
-            currentUser === '' ?
-              (
-                <Dropdown id={COMPONENT_IDS.NAVBAR_LOGIN_DROPDOWN} text="Login" pointing="top right"
-                  icon={'user md'}>
-                  <Dropdown.Menu>
-                    <Dropdown.Item id={COMPONENT_IDS.NAVBAR_LOGIN_DROPDOWN_SIGN_IN} icon="user" text="Sign In"
-                      as={NavLink} exact to="/signin"/>
-                    <Dropdown.Item id={COMPONENT_IDS.NAVBAR_LOGIN_DROPDOWN_SIGN_UP} icon="add user"
-                      text="Sign Up"
-                      as={NavLink} exact to="/signup"/>
-                  </Dropdown.Menu>
-                </Dropdown>
-              ) :
-              (
-                <Dropdown id={COMPONENT_IDS.NAVBAR_CURRENT_USER} text={currentUser} pointing="top right"
-                  icon={'user'}>
-                  <Dropdown.Menu>
-                    <Dropdown.Item id={COMPONENT_IDS.NAVBAR_SIGN_OUT} icon="sign out" text="Sign Out"
-                      as={NavLink} exact
-                      to="/signout"/>
-                  </Dropdown.Menu>
-                </Dropdown>
-              )
-          }
-        </Menu.Item>
-      </Menu>
-    </div>
+          <Menu.Item id={COMPONENT_IDS.NAVBAR_DISPENSE} as={NavLink} activeClassName="active" exact to="/dispense" key='dispense'>
+            Dispense Inventory
+            <Icon name='pills'/>
+          </Menu.Item>
+          <Menu.Item id={COMPONENT_IDS.NAVBAR_STATUS} as={NavLink} activeClassName="active" exact to="/status" key='status'>
+            Inventory Log
+            <Icon name='archive'/>
+          </Menu.Item>
+          <Menu.Item id={COMPONENT_IDS.NAVBAR_DISPENSE_LOG} as={NavLink} activeClassName="active" exact to="/dispense-log" key='dispense-log'>
+            Dispense Log
+            <Icon name='book'/>
+          </Menu.Item>
+        </>
+      }
+      {
+        Roles.userIsInRole(Meteor.userId(), [ROLE.ADMIN]) &&
+        <Dropdown id={COMPONENT_IDS.NAVBAR_MANAGE_DROPDOWN} item text="Manage" key="manage">
+          <Dropdown.Menu>
+            <Dropdown.Item id={COMPONENT_IDS.NAVBAR_MANAGE_DROPDOWNS} as={NavLink} exact to="/manage-dropdowns" key='manage-dropdowns' content="Dropdowns" />
+            <Dropdown.Item id={COMPONENT_IDS.NAVBAR_MANAGE_DROPDOWN_DATABASE} key="manage-database" as={NavLink} exact to="/manage-database" content="Database" />
+            <Dropdown.Item id={COMPONENT_IDS.NAVBAR_MANAGE_DROPDOWN_USERS} key="manage-users" as={NavLink} exact to="/manage-users" content="Users"/>
+            <Dropdown.Item key="manage-new-users" as={NavLink} exact to="/manage-new-users" content="New Users"/>
+          </Dropdown.Menu>
+        </Dropdown>
+      }
+      {/* <Menu.Item id={COMPONENT_IDS.NAVBAR_STATUS_NOTIFICATION} position="right">
+        <StatusNotification/>
+      </Menu.Item> */}
+      <Menu.Item position='right'>
+        <Dropdown id={COMPONENT_IDS.NAVBAR_CURRENT_USER} text={currentUser} pointing="top right" icon={'user'}>
+          <Dropdown.Menu>
+            <Dropdown.Item id={COMPONENT_IDS.NAVBAR_SIGN_OUT} icon="sign out" text="Sign Out" as={NavLink} exact to="/signout"/>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Menu.Item>
+    </Menu>
   );
 };
 
 // Declare the types of all properties.
-NavBar.propTypes =
-    {
-      currentUser: PropTypes.string,
-    };
+NavBar.propTypes = {
+  currentUser: PropTypes.string,
+};
 
 // withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
 const NavBarContainer = withTracker(() => {
